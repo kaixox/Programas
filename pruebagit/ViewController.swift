453f7131f00ce0b115764ed5c720a732588d267f
Nagore <nagore@macbook-pro-de-informatica.local>
2016-07-21 02:07:54 +0200

    commit2

--- pruebagit/ViewController.swift
+++ pruebagit/ViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  pruebagit
-//
-//  Created by Nagore on 21/7/16.
-//  Copyright © 2016 Nagore. All rights reserved.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+for i in 0...100{
+    if i % 5 == 0{
+        print("\(i) + Bingo!!!")
+    };if i % 2 == 0{
+        print("\(i) + par!!!")
+    };if i % 2 != 0{
+        print("\(i) + impar!!!")
+    };if i >= 30 && i <= 40{
+        print("\(i) + Viva Swift!!!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
